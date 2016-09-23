d563fcb3d2c7f19830ceb9322cc5aabbcd86fee6
hsyou <realhs01@gmail.com>
2016-09-23 10:09:04 +0900

    22일

---
+++ app/src/main/java/com/example/sid/sidmobile/VolleySingleton.java
@@ -0,0 +1,56 @@
+package com.example.sid.sidmobile;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by hs on 2016-09-22.
+ */
+
+public class VolleySingleton {
+    private static VolleySingleton mInstance = null;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static Context mCtx;
+
+    private VolleySingleton(Context context){
+        mCtx=context;
+        mRequestQueue = Volley.newRequestQueue(context);
+
+        mImageLoader = new ImageLoader(this.mRequestQueue, new ImageLoader.ImageCache() {
+            private final LruCache <String, Bitmap> mCache = new LruCache<String,Bitmap>(10);
+
+            public void putBitmap(String url, Bitmap bitmap) {
+                mCache.put(url, bitmap);
+            }
+            public Bitmap getBitmap(String url) {
+                return mCache.get(url);
+            }
+        });
+    }
+
+    public static VolleySingleton getInstance(Context context){
+        if(mInstance == null){
+            mInstance = new VolleySingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        return this.mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader(){
+        return this.mImageLoader;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
