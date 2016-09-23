cde0fd2df5c5d8c539d4a5e0e86e6c55f4734417
hsyou <realhs01@gmail.com>
2016-09-23 10:06:53 +0900

    22일

--- app/src/main/java/com/example/sid/sidmobile/MainActivity.java
+++ app/src/main/java/com/example/sid/sidmobile/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.sid.sidmobile;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +12,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.StringRequest;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private final String url = "http://www.google.com";
+    private final String imgurl = "http://www.google.co.kr/logos/doodles/2014/anna-freuds-119th-birthday-5664856720015360-hp.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +53,52 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+        final ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+
+        // Get a RequestQueue
+        RequestQueue queue = VolleySingleton.getInstance(this.getApplicationContext()).
+                getRequestQueue();
+
+        // Formulate the request and handle the response.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        mTextView.setText("Response is: " + response.substring(0, 500));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mTextView.setText(error.toString());
+                    }
+                });
+
+
+        ImageRequest imageRequest = new ImageRequest(imgurl,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        mImageView.setImageBitmap(bitmap);
+                    }
+                }, 0, 0, null,
+                new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        mImageView.setImageResource(R.drawable.ic_launcher);
+                    }
+                });
+        // Access the RequestQueue through your singleton class.
+        VolleySingleton.getInstance(this).addToRequestQueue(imageRequest);
+
+        // Add a request (in this example, called stringRequest) to your RequestQueue.
+        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -92,9 +150,9 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_base) {
 
-        } else if(id==R.id.nav_image ){
+        } else if (id == R.id.nav_image) {
 
-        } else if (id==R.id.nav_proxy){
+        } else if (id == R.id.nav_proxy) {
 
         }
 
